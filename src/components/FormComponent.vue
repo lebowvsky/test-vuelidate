97fb24a8214ffea211f0e6dffc3f27e371643671
Brice <lebowvsky@gmail.com>
2022-09-16 14:09:57 +0200

    Init

---
+++ src/components/FormComponent.vue
@@ -0,0 +1,18 @@
+<template>
+  <form class="form">
+    <div class="fieldset">
+      <label for="" class="f-label">Name</label>
+      <input type="text" :value="name" @input="setName">
+    </div>
+  </form>
+</template>
+<script lang="ts">
+import { defineComponent } from 'vue'
+
+export default defineComponent({
+  
+})
+</script>
+<style scoped>
+
+</style>
